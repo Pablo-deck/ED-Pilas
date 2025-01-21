e6719a5ac2b3426d0523eeca5801a883b9fea332
Pablo <UPN@ULCLAB1304>
2025-01-21 11:10:25 -0500

    avance 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace S3_1_2 {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing&&(components!=null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnApilar = new System.Windows.Forms.Button();
+            this.lsNombres = new System.Windows.Forms.ListBox();
+            this.btnDesapilar = new System.Windows.Forms.Button();
+            this.btnVistazo = new System.Windows.Forms.Button();
+            this.btnElementos = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PILA - STACK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(113, 40);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(184, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnApilar
+            // 
+            this.btnApilar.Location = new System.Drawing.Point(25, 76);
+            this.btnApilar.Name = "btnApilar";
+            this.btnApilar.Size = new System.Drawing.Size(82, 23);
+            this.btnApilar.TabIndex = 3;
+            this.btnApilar.Text = "Apilar";
+            this.btnApilar.UseVisualStyleBackColor = true;
+            // 
+            // lsNombres
+            // 
+            this.lsNombres.FormattingEnabled = true;
+            this.lsNombres.Location = new System.Drawing.Point(113, 76);
+            this.lsNombres.Name = "lsNombres";
+            this.lsNombres.Size = new System.Drawing.Size(184, 199);
+            this.lsNombres.TabIndex = 4;
+            // 
+            // btnDesapilar
+            // 
+            this.btnDesapilar.Location = new System.Drawing.Point(25, 105);
+            this.btnDesapilar.Name = "btnDesapilar";
+            this.btnDesapilar.Size = new System.Drawing.Size(82, 23);
+            this.btnDesapilar.TabIndex = 5;
+            this.btnDesapilar.Text = "Desapilar";
+            this.btnDesapilar.UseVisualStyleBackColor = true;
+            // 
+            // btnVistazo
+            // 
+            this.btnVistazo.Location = new System.Drawing.Point(25, 134);
+            this.btnVistazo.Name = "btnVistazo";
+            this.btnVistazo.Size = new System.Drawing.Size(82, 23);
+            this.btnVistazo.TabIndex = 6;
+            this.btnVistazo.Text = "Vistazo";
+            this.btnVistazo.UseVisualStyleBackColor = true;
+            // 
+            // btnElementos
+            // 
+            this.btnElementos.Location = new System.Drawing.Point(25, 163);
+            this.btnElementos.Name = "btnElementos";
+            this.btnElementos.Size = new System.Drawing.Size(82, 23);
+            this.btnElementos.TabIndex = 7;
+            this.btnElementos.Text = "# Elementos";
+            this.btnElementos.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 303);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnElementos);
+            this.Controls.Add(this.btnVistazo);
+            this.Controls.Add(this.btnDesapilar);
+            this.Controls.Add(this.lsNombres);
+            this.Controls.Add(this.btnApilar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnApilar;
+        private System.Windows.Forms.ListBox lsNombres;
+        private System.Windows.Forms.Button btnDesapilar;
+        private System.Windows.Forms.Button btnVistazo;
+        private System.Windows.Forms.Button btnElementos;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
